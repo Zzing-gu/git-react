5e3da15b378b04c618eec57e3e5ff0afb2927adb
hosuk <kirklayer@gmail.com>
2019-07-10 14:57:44 +0900

    dragdrop

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import DnD from './dnd'
+
+import Axios from 'axios'
+
+
+const CreateAndInit = () => {
+  var dir = prompt("생성하실 레포지터리의 경로를 정해주세요(예: where/is/repo)")
+  InitRequest(dir)
+}
+
+const InitRequest = async (dir) => {
+  await Axios.get(`http://127.0.0.1:8000/git/init?path=${dir}`)
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={CreateAndInit}>InitBareRepository</button>
+      <DnD />
     </div>
   );
 }
---
+++ src/dnd.js
@@ -0,0 +1,65 @@
+import React, { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+
+import Axios from 'axios'
+
+export default function MyDropzone() {
+
+
+    const Filetest = async (acceptedFiles) => {
+        console.log(acceptedFiles[0])
+        var filename = acceptedFiles[0].name
+
+        var formData  = new FormData();
+
+        formData.append('name', acceptedFiles[0])
+
+        var config = {
+            // Accept: 'application/json, text/plain, */*',
+            // 'content-type': '*/*',
+            
+            headers: {
+                //ContentDisposition: `attachment; filename=${filename}`,
+                'Content-Type': 'multipart/form-data',
+                //'contenttype': '*/*',
+                'Content-Disposition': `attachment; filename=${filename}`,
+                
+            }
+        }
+
+
+
+        await Axios.put(`http://127.0.0.1:8000/git/test/`, formData, config)
+            .then(function (response) {
+                // handle success
+                console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+    }
+
+
+
+    const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+        Filetest(acceptedFiles)
+    }, [])
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+    return (
+        <div {...getRootProps()}>
+            <input type="file" id="image_uploads" name="image_uploads" ></input>
+            <input {...getInputProps()} />
+            {
+                isDragActive ?
+                    <p>Drop the files here ...</p> :
+                    <p>Drag 'n' drop some files here, or click to select files</p>
+            }
+
+
+            
+        </div>
+    )
+}
